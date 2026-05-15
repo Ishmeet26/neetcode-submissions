e90d44a642ce874c66a45300a84078ec2219b6bf
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-05-16 00:14:26 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+       if(nums.length ===0){
+        return 0;
+       }
+
+       nums.sort((a,b)=> a-b);
+        let prevCurr = 1;
+        let curr = 1;
+       for(let i = 1 ; i<nums.length; i++){
+        if(nums[i] === nums[i-1]+1){
+            curr++;
+        }else if(nums[i] === nums[i-1]){
+            continue;
+        }else if(nums[i] > nums[i-1]+1){
+            prevCurr = Math.max(prevCurr, curr);
+            curr = 1;
+        }
+
+       }
+       return Math.max(prevCurr, curr);
+    }
+}
