d12758c0909eb8479cda49ffd0c46d9cf817b187
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-06-24 00:46:25 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let res = new Array(nums.length).fill(1);
+
+        for (let i = 1; i<nums.length; i++){
+            res[i] = res[i-1] * nums[i-1];
+        }
+
+        console.log("prefix " , res);
+
+        let postfix= 1;
+        for(let i = nums.length-1 ; i >=0; i--){
+            res[i] = postfix * res[i];
+            postfix*=nums[i];
+        }
+    console.log(res);
+return res;
+
+    }
+}
