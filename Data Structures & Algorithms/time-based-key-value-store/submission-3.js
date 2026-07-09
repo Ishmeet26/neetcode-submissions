244ff3b5a8d9ac25ec0c054f8bd24611320f9cda
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-07-09 13:49:12 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.js
@@ -0,0 +1,46 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.has(key)) {
+            this.keyStore.set(key, []);
+        }
+        this.keyStore.get(key).push([value, timestamp]);
+        console.log(this.keyStore);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if (!this.keyStore.has(key)) return "";
+        let list = this.keyStore.get(key);
+        if (list.length === 0) return "";
+        let l = 0;
+        let r = list.length - 1;
+        let res = "";
+        while (l <= r) {
+            let mid = Math.floor(l + (r - l) / 2);
+
+            if (list[mid][1] === timestamp) {
+                return list[mid][0];
+            } else if (list[mid][1] < timestamp) {
+                res = list[mid][0];
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res.length ? res : "";
+    }
+}
