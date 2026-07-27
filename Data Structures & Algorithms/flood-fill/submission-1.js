eb6a2ba9f765cad54a79aa7906a396d4eebc8169
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-07-28 00:13:20 +0530

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {number[][]} image
+     * @param {number} sr
+     * @param {number} sc
+     * @param {number} color
+     * @return {number[][]}
+     */
+    floodFill(image, sr, sc, color) {
+        let queue = [];
+        let front = 0;
+        let stVal = image[sr][sc];
+        //if starting point colour and colour is same, then no colour will be done
+        if (stVal === color) {
+            return image;
+        }
+        queue.push([sr, sc]);
+        image[sr][sc] = color;
+        let n = image.length;
+        let m = image[0].length;
+        while (front < queue.length) {
+            let row = queue[front][0];
+            let col = queue[front][1];
+            front++;
+
+            for (let delRow = -1; delRow <= 1; delRow++) {
+                for (let delCol = -1; delCol <= 1; delCol++) {
+                    let nRow = delRow + row;
+                    let nCol = delCol + col;
+                    if (Math.abs(delRow) + Math.abs(delCol) !== 1) {
+                        continue;
+                    }
+
+                    if (
+                        nRow >= 0 &&
+                        nRow < n &&
+                        nCol >= 0 &&
+                        nCol < m &&
+                        image[nRow][nCol] === stVal
+                    ) {
+                        image[nRow][nCol] = color;
+                        queue.push([nRow, nCol]);
+                    }
+                }
+            }
+        }
+        return image;
+    }
+}
