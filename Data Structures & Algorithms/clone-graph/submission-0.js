871c575bb791645f300f9eaa6cc913ded3a14a8a
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-07-29 22:06:52 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.js
@@ -0,0 +1,35 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        let oldToNew = new Map();
+
+        function dfs(node){
+            if(oldToNew.has(node)){
+                return oldToNew.get(node);
+            }
+
+            let copy = new Node(node.val);
+            oldToNew.set(node, copy);
+
+            for(let neighbor of node.neighbors){
+                copy.neighbors.push(dfs(neighbor))
+            }
+            return copy;
+        }
+        return node ? dfs(node) : null;
+    }
+}
+ 
