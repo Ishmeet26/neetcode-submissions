7f4bf1df085b8bc82f9672cb05d1cc46ed1a7d42
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-08-06 01:42:14 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if (n === 0) return true;
+        if (edges.length > n - 1) {
+            return false;
+        }
+        let adj = Array.from({ length: n }, () => []);
+        let visit = new Set();
+        for (let [u, v] of edges) {
+            adj[u].push(v);
+            adj[v].push(u);
+        }
+
+        function dfs(node, parent) {
+            visit.add(node);
+            for (let adjNode of adj[node]) {
+                if (!visit.has(adjNode)) {
+                    if (!dfs(adjNode, node)) return false;
+                } else if (adjNode !== parent) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return dfs(0, -1) && n === visit.size;
+    }
+}
