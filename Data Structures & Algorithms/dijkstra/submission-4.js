37020f052504be95c30228256781a0e8a6046c6b
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-08-16 02:02:15 +0530

    Add: dijkstra - submission-4

---
+++ Data Structures & Algorithms/dijkstra/submission-4.js
@@ -0,0 +1,37 @@
+/**
+ * const PriorityQueue = require('priority-queue-js');
+ */
+
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @param {number} src
+     * @returns {Object}
+     */
+    shortestPath(n, edges, src) {
+        let adj = Array.from({ length: n }, () => []);
+        for (let [u, v, wt] of edges) {
+            adj[u].push([v, wt]);
+        }
+
+        let dist = new Array(n).fill(Infinity);
+        dist[src] = 0;
+
+        //using priority queue 
+        let pq = new PriorityQueue((a, b) => a - b);
+        pq.push([0, src]);
+
+        while (pq.size()) {
+            let [currDist, node] = pq.pop();
+
+            for (let [adjNode, wt] of adj[node]) {
+                if (currDist + wt < dist[adjNode]) {
+                    dist[adjNode] = currDist + wt;
+                    pq.push([dist[adjNode], adjNode]);
+                }
+            }
+        }
+        return dist.map((d) => (d === Infinity ? -1 : d));
+    }
+}
