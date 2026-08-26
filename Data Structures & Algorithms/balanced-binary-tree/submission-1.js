e9c1e4aaad5c2bc7979b4bcd48acb5b5c1a5d4cc
Ishmeet Singh <68912969+Ishmeet26@users.noreply.github.com>
2026-08-26 23:44:05 +0530

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        if (!root) return true;
+
+        function dfs(node) {
+            if (!node) return 0;
+
+            let lh = dfs(node.left);
+            if (lh === -1) return -1;
+            let rh = dfs(node.right);
+            if (rh === -1) return -1;
+            if (Math.abs(lh - rh) > 1) return -1;
+
+            return 1 + Math.max(lh, rh);
+        }
+
+        let ans = dfs(root);
+
+        return ans === -1 ? false : true;
+    }
+}
